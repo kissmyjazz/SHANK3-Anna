c6843826fa202f306de7f24fb856465239d34970
Matrov <matrovd2@nih.gov>
2021-11-11 13:45:05 -0500

    Modified code to work with "step" protocol data

--- R/ingest_data.R
+++ R/ingest_data.R
@@ -3,9 +3,9 @@ library(tidyverse)
 #  each `\` needs to be escaped once by another `\`, so UNC addresses are
 # required to start with four \\\\-s
 
-file_path <- "\\\\nimhlabstore-a.nimh.nih.gov\\lab\\SBN\\Chudasama\\Anna\\SHANK\\Trialwise Data Updated 2021-08-10"
+file_path <- "\\\\nimhlabstore-a.nimh.nih.gov\\lab\\SBN\\Chudasama\\Anna\\SHANK\\Trialwise Updated 2021-10-08"
 
-test_file_path <- "\\\\nimhlabstore-a.nimh.nih.gov\\lab\\SBN\\Chudasama\\Anna\\SHANK\\test_sandbox"
+# test_file_path <- "\\\\nimhlabstore-a.nimh.nih.gov\\lab\\SBN\\Chudasama\\Anna\\SHANK\\test_sandbox"
 
 ingest_data <- function(fpath, output_name = NULL) {
   if(!dir.exists(fpath)) {
@@ -18,29 +18,47 @@ ingest_data <- function(fpath, output_name = NULL) {
   # list of file names
   fname <- purrr::map(flist, basename)
 
-  # column names
-  varnames <- c("skip", "skip2", "trial_nr", "correct_trial", "incorrect_trial",
-                "omission_trial", "correction_trial", "cue_position_rewarded",
-                "cue_position_nonrewarded", "initiation_touch_correct",
-                "initiation_touch_incorrect", "initiation_touch_blank",
-                "perseverative_touch_correct", "perseverative_touch_incorrect",
-                "perseverative_touch_blank", "consume_touch_correct",
-                "consume_touch_incorrect", "consume_touch_blank",
-                "timeout_touch_correct", "timeout_touch_incorrect",
-                "timeout_touch_blank", "iti_touch_correct", "iti_touch_incorrect",
-                "iti_touch_blank", "response_latency",
-                "reward_latency", "initiation_latency")
+  # column names for a scipt with changing pairs of stimuli
+  varnames_step <- c("skip", "skip2", "trial_nr", "correct_trial", "incorrect_trial",
+                     "omission_trial", "correction_trial", "cue_position_rewarded",
+                     "cue_position_nonrewarded", "initiation_touch_correct",
+                     "initiation_touch_incorrect", "initiation_touch_blank",
+                     "perseverative_touch_correct", "perseverative_touch_incorrect",
+                     "perseverative_touch_blank", "consume_touch_correct",
+                     "consume_touch_incorrect", "consume_touch_blank",
+                     "timeout_touch_correct", "timeout_touch_incorrect",
+                     "timeout_touch_blank", "iti_touch_correct", "iti_touch_incorrect",
+                     "iti_touch_blank", "response_latency",
+                     "reward_latency", "pair", "initiation_latency")
 
-  df <- flist %>% purrr::map(read_csv, skip = 13,
-                                       col_names = varnames,
-                                       na = "",
-                                       trim_ws = TRUE,
-                             col_types = cols())  %>%
+  # column names for a previous version of the script
+  varnames_old <- varnames_step[-27]
+
+  # partial functions of read_csv and str_detect to read in the proper number
+  # of columns depending on the script
+
+  read_csv_step <- purrr::partial(readr::read_csv, col_names = varnames_step)
+  read_csv_old <- purrr::partial(readr::read_csv, col_names = varnames_old)
+  detect_step <- purrr::partial(stringr::str_detect, pattern = "_step")
+  add_pair <- purrr::partial(tibble::add_column, pair = NA,
+                             .before = "initiation_latency")
+
+  # Logical vector testing for the presence of "_step" in filenames
+  detected_step <- detect_step(fname)
+
+  df <- flist %>% purrr::map_if(detected_step,
+                                read_csv_step,
+                                skip = 13,
+                                na = "",
+                                trim_ws = TRUE,
+                                col_types = cols(),
+                                .else = read_csv_old) %>%
+    purrr::map_if(!detected_step, add_pair) %>%
     purrr::set_names(fname) %>%
     dplyr::bind_rows(.id = "file_name") %>%
     dplyr::mutate(skip = NULL, skip2 = NULL,
                   fname = purrr::map(file_name, stringr::str_split_fixed,
-                              pattern = "[_\\.]", n = 6),
+                                     pattern = "[_\\.]", n = 6),
                   date = purrr::map_chr(fname, 1) %>% as.Date(format = "%Y-%m-%d"),
                   animal_id = purrr::map_chr(fname, 2) %>% factor(),
                   sex = purrr::map_chr(fname, 3) %>% factor(),
@@ -49,7 +67,13 @@ ingest_data <- function(fpath, output_name = NULL) {
                   response_latency = dplyr::na_if(response_latency, 0),
                   reward_latency = dplyr::na_if(reward_latency, 0),
                   initiation_latency = dplyr::na_if(initiation_latency, 0),
-                  fname = NULL) %>%
+                  fname = NULL,
+                  pair = dplyr::case_when(protocol == "old-SD" ~ 1,
+                                          protocol == "new-SD" |
+                                            protocol == "reversal" |
+                                            protocol ==  "space" ~ 2,
+                                          TRUE ~ pair),
+                  pair = as.integer(pair)) %>%
     dplyr::select(date:protocol, everything()) %>%
     # filter out rows with lots of missing values
     dplyr::filter(!rowSums(is.na(.)) >= 20) %>%
@@ -59,11 +83,18 @@ ingest_data <- function(fpath, output_name = NULL) {
   if(!is.null(output_name)) {
     dir.create(paste0(fpath, "\\joined_data"))
     Sys.chmod(list.dirs(fpath), "777")
+
+    # writes .rds file
     out_path <- paste0(fpath, "\\joined_data", paste0("\\", output_name, ".rds"))
-    write_rds(df, out_path)
+    readr::write_rds(df, out_path)
+
+    # writes .csv file
+    out_path <- paste0(fpath, "\\joined_data", paste0("\\", output_name, ".csv"))
+    readr::write_csv(df, out_path)
   }
   invisible(df)
 }
 
-df <- ingest_data(test_file_path, "test_data")
-write_csv(df, "\\\\nimhlabstore-a.nimh.nih.gov\\lab\\SBN\\Chudasama\\Anna\\SHANK\\test_sandbox\\joined_data.csv")
+df <- ingest_data(file_path, "combined_data")
+
+# df <- ingest_data(test_file_path, "test_data")
